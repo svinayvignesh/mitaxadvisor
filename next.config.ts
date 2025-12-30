0b238a16f459f8833592b17b70ff406294a2b60d
Vinay Shasi <77236929+svinayvignesh@users.noreply.github.com>
2025-12-30 13:50:06 -0500

    Update Next.js configuration for export and base path

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+    output: 'export',
+    basePath: '/mitaxadvisor',
+    images: {
+          unoptimized: true,
+        },
 };
 
 export default nextConfig;
