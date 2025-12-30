2e5b2aa897b32a8691d5bd5a5c55bf9661975e17
Vinay Shasi <77236929+svinayvignesh@users.noreply.github.com>
2025-12-30 17:02:37 -0500

    Remove basePath configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
     output: 'export',
-    basePath: '/mitaxadvisor',
     images: {
           unoptimized: true,
         },
